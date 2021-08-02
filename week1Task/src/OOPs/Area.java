adb3a7952dfe2d9161da2a1a25ae9d1f5410ca01
maheshkumarsingh <sm.singhmahesh@gmail.com>
2021-08-02 13:43:20 +0530

    all task done

--- week1Task/src/OOPs/Area.java
+++ week1Task/src/OOPs/Area.java
@@ -1,7 +1,8 @@
 package OOPs;
 
 public class Area {
-private double height;
+
+	/*private double height;
 private double width;
 private double side;
 private double parallelA;
@@ -36,8 +37,28 @@ public Area(double height, double parallelA, double parallelB) {
 public static void main(String[] args) {
 	Area ar(10,20);
 	Area ar1(10.0);
+}*/
+	
+public static double Area(double side)
+{
+	return side*side;
 }
 
+public static double Area(double height,double width)
+{
+	return height*width;
+}
+
+public static double Area(double a,double b,double h)
+{
+	return (h*(a+b)/2);
+}
+
+public static void main(String[] args) {
+	System.out.println("Area of Square:"+Area(10));
+	System.out.println("area of rect:"+Area(10,15));
+	System.out.println("area of Trapz:"+Area(10,15,2));
+}
 
 
 
